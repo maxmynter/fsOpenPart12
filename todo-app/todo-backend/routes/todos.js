0fc017b28c614cf6d9a22483e81cdc9e3735b3b7
Max Mynter <maxmynter@me.com>
2023-08-21 23:11:45 +0200

    Ex.12.7

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,49 +1,67 @@
-const express = require('express');
-const { Todo } = require('../mongo')
-const router = express.Router();
+const express = require("express")
+const { Todo } = require("../mongo")
+const router = express.Router()
 
 /* GET todos listing. */
-router.get('/', async (_, res) => {
+router.get("/", async (_, res) => {
   const todos = await Todo.find({})
-  res.send(todos);
-});
+  res.send(todos)
+})
 
 /* POST todo to listing. */
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const todo = await Todo.create({
     text: req.body.text,
-    done: false
+    done: false,
   })
-  res.send(todo);
-});
+  res.send(todo)
+})
 
-const singleRouter = express.Router();
+const singleRouter = express.Router()
 
 const findByIdMiddleware = async (req, res, next) => {
   const { id } = req.params
-  req.todo = await Todo.findById(id)
+  try {
+    req.todo = await Todo.findById(id)
+  } catch (error) {
+    console.log("Could not find entry with ID. Error: ", error)
+  }
   if (!req.todo) return res.sendStatus(404)
 
   next()
 }
 
 /* DELETE todo. */
-singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
-  res.sendStatus(200);
-});
+singleRouter.delete("/", async (req, res) => {
+  await req.todo.delete()
+  res.sendStatus(200)
+})
 
 /* GET todo. */
-singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
-});
+singleRouter.get("/", async (req, res) => {
+  const todo = req.todo
+  if (!todo) {
+    return res.status(500).json({ message: "Error getting todo" })
+  } else {
+    return res.json(todo)
+  }
+})
 
 /* PUT todo. */
-singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
-});
+singleRouter.put("/", async (req, res) => {
+  try {
+    await Todo.findOneAndUpdate({ _id: req.todo.id }, { $set: req.body })
+    console.log(
+      "🚀 ~ file: todos.js:54 ~ singleRouter.put ~ req.body:",
+      req.body
+    )
+    res.send(200)
+  } catch (error) {
+    console.log("Error updating todo. Error:", error)
+    res.sendStatus(400)
+  }
+})
 
-router.use('/:id', findByIdMiddleware, singleRouter)
+router.use("/:id", findByIdMiddleware, singleRouter)
 
-
-module.exports = router;
+module.exports = router
